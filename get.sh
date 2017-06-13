177f4a1c925398245b3b0397b12b69c38606a2c5
Alexey Radul <axch@mit.edu>
2017-06-13 15:04:14 -0400

    Candidate scripts for getting files to and from individual instances.

---
+++ get.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -Ceu
+
+user=$1
+shift
+source=$1
+shift
+dest=$1
+shift
+
+scp -i bch20170503-ec2.pem -o userknownhostsfile=known_hosts \
+  "ubuntu@ssh.$user.stack.probcomp.net:$source" "$dest" $@
