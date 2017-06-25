704cf19eb360cf3ab763a9842918358e890649d6
Alexey Radul <axch@mit.edu>
2017-06-25 11:37:13 -0400

    Draft automation for building a self-contained tarball with the desired Docker image.

---
+++ release-container.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -Ceu
+
+# Reset the working directory to the script's path
+my_abs_path=$(readlink -f "$0")
+root_dirname=$(dirname "$my_abs_path")
+echo "$root_dirname"
+cd "$root_dirname"
+
+release=${1:-0.1}
+
+# Build the bare release container as a base
+docker build -t probcomp/base -f docker/ubuntu1604-jupyter docker
+
+# Collect the content to release into a directory
+rm -rf release-playpen
+mkdir -p release-playpen
+release-archive.sh "$release"
+cp "probcomp-stack-$release.zip" release-playpen
+(cd release-playpen && unzip "probcomp-stack-$release.zip")
+cp docker/ubuntu1604-jupyter-full release-playpen
+
+# Build the full release container
+docker build -t "probcomp/stack-release:$release" \
+       -f release-playpen/ubuntu1604-jupyter-full \
+       release-playpen/
+
+# Export it
+docker save --output="probcomp-stack-full-$release.tar" \
+       "probcomp/stack-release:$release"
