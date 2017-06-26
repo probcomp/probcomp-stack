048e21d0fc08e71e8d06e03d2c20355ce9488237
Alexey Radul <axch@mit.edu>
2017-06-26 18:09:21 -0400

    Right.  The user and stack definitions go where $i is set.

--- fleet.sh
+++ fleet.sh
@@ -16,26 +16,29 @@ instance=$4
 prefix=oreilly
 ami_id=ami-751b2c63
 
-user=$prefix-$i
-stack=probcomp-stack-$user
-
 case $action in
     create|update)
         for i in `seq $from $to`
         do
+            user=$prefix-$i
+            stack=probcomp-stack-$user
             echo "`echo ${action%e} | tr cu CU`ing $stack"
-            ./stack-start.sh $action $prefix-$i $instance $ami_id &
+            ./stack-start.sh $action $user $instance $ami_id &
             sleep 1
         done
         wait
         for i in `seq $from $to`
         do
+            user=$prefix-$i
+            stack=probcomp-stack-$user
             echo "Waiting for $stack to come up"
             ./aws/stackwait.sh $stack || true
             sleep 1
         done
         for i in `seq $from $to`
         do
+            user=$prefix-$i
+            stack=probcomp-stack-$user
             echo "Finalizing $stack"
             ./stack-finish.sh $user || true
             sleep 1
