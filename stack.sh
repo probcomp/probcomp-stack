2f48d98a465473a3c4d003edb0672a3b8c554498
Alexey Radul <axch@mit.edu>
2017-06-25 20:04:10 -0400

    Teach the local stack script to take an optional ami argument (is this how I should do that?)

--- stack.sh
+++ stack.sh
@@ -3,6 +3,7 @@
 action=$1
 user=$2
 instance=$3
+ami_id=${4:-ami-0f6b2119}
 
 aws cloudformation $action-stack \
     --stack-name probcomp-stack-$user \
@@ -12,4 +13,5 @@ aws cloudformation $action-stack \
       ParameterKey=KeyName,ParameterValue=bch20170503-ec2 \
       ParameterKey=CertificateArn,ParameterValue=arn:aws:acm:us-east-1:590421120965:certificate/f3ac3e7b-b8fd-4bb8-9bb7-c2f9299c9233 \
       ParameterKey=Hostname,ParameterValue=$user.stack \
-      ParameterKey=Zone,ParameterValue=probcomp.net.
+      ParameterKey=Zone,ParameterValue=probcomp.net. \
+      ParameterKey=BaseAMI,ParameterValue=$ami_id
