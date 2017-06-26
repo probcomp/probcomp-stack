0f24d3c641bbe7863be4c0932e94b00b0e8225a1
Alexey Radul <axch@mit.edu>
2017-06-26 18:48:00 -0400

    Create the known hosts directory for good measure.

--- stack-finish.sh
+++ stack-finish.sh
@@ -5,6 +5,7 @@ set -Ceu
 user=$1
 
 # Set the machine's hostname.
+mkdir -p known_hosts/
 rm -f known_hosts/$user
 ./hostkey.sh $user > known_hosts/$user
 ./put.sh $user set-hostname.sh /home/ubuntu/set-hostname.sh -o StrictHostKeyChecking=yes
