bf4f2a3a1b0ee63f21b98fe947dd90f19c3675ae
Alexey Radul <axch@mit.edu>
2017-06-26 17:33:43 -0400

    Split off finishing a stack into its own script, so it can be called independently.

---
+++ stack-finish.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -Ceu
+
+# Set the machine's hostname.
+rm -f known_hosts.$user
+./hostkey.sh $user > known_hosts.$user
+./put.sh $user set-hostname.sh /home/ubuntu/set-hostname.sh
+./login.sh $user chmod 755 set-hostname.sh
+./login.sh $user ./set-hostname.sh $user.stack.probcomp.net
--- stack.sh
+++ stack.sh
@@ -25,9 +25,4 @@ aws cloudformation $action-stack \
 
 ./aws/stackwait.sh probcomp-stack-$user
 
-# Set the machine's hostname.
-rm -f known_hosts.$user
-./hostkey.sh $user > known_hosts.$user
-./put.sh $user set-hostname.sh /home/ubuntu/set-hostname.sh
-./login.sh $user chmod 755 set-hostname.sh
-./login.sh $user ./set-hostname.sh $user.stack.probcomp.net
+./stack-finish.sh $user
