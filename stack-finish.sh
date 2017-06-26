26453558eeb48f06ed9d254eb582cc7f953ee53b
Alexey Radul <axch@mit.edu>
2017-06-26 18:08:24 -0400

    Enforce strict host key checking so that finishing doesn't hang waiting for a key that's not there.

--- stack-finish.sh
+++ stack-finish.sh
@@ -7,6 +7,6 @@ user=$1
 # Set the machine's hostname.
 rm -f known_hosts/$user
 ./hostkey.sh $user > known_hosts/$user
-./put.sh $user set-hostname.sh /home/ubuntu/set-hostname.sh
+./put.sh $user set-hostname.sh /home/ubuntu/set-hostname.sh -o StrictHostKeyChecking=yes
 ./login.sh $user chmod 755 set-hostname.sh
 ./login.sh $user ./set-hostname.sh $user.stack.probcomp.net
