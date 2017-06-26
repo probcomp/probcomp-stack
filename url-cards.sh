b09f5ed85170ac6cb766e74a6cc663addc8a8d12
Alexey Radul <axch@mit.edu>
2017-06-26 15:45:19 -0400

    Re-do the URL cards for the O'Reilly workshop.

--- url-cards.sh
+++ url-cards.sh
@@ -9,30 +9,46 @@
 set -eu
 
 echo "" > url-cards.txt
-(for USER in $@
+(for i in `seq 1 4 400`
 do cat <<EOF >> url-cards.txt
+MIT Probabilistic Computing Project
+O'Reilly Artificial Intelligence Conference
+New York, June 27, 2017
+
+https://oreilly-$i.stack.probcomp.net
+Password: `cat oreilly-passwords/$i.passwd`
 
 
 
 
+MIT Probabilistic Computing Project
+O'Reilly Artificial Intelligence Conference
+New York, June 27, 2017
+
+https://oreilly-$((i+1)).stack.probcomp.net
+Password: `cat oreilly-passwords/$((i+1)).passwd`
 
 
 
 
+MIT Probabilistic Computing Project
+O'Reilly Artificial Intelligence Conference
+New York, June 27, 2017
+
+https://oreilly-$((i+2)).stack.probcomp.net
+Password: `cat oreilly-passwords/$((i+2)).passwd`
 
 
 
 
+MIT Probabilistic Computing Project
+O'Reilly Artificial Intelligence Conference
+New York, June 27, 2017
 
-
-
-
-
-
-
-https://$USER.stack.probcomp.net
+https://oreilly-$((i+3)).stack.probcomp.net
+Password: `cat oreilly-passwords/$((i+3)).passwd`
 
 EOF
 done)
 
-enscript -fCourier-Bold24 url-cards.txt -o url-cards.ps -b ''
+enscript -fCourier-Bold16 url-cards.txt -o url-cards.ps -b ''
