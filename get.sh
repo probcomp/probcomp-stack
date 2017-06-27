c53c3de7bd7601d58cd6eb3881f5f65c0b1d3dcf
Alexey Radul <axch@mit.edu>
2017-06-26 20:07:06 -0400

    Stop checking host IPs in get and put also.

--- get.sh
+++ get.sh
@@ -9,6 +9,8 @@ shift
 dest=$1
 shift
 
-scp -i bch20170503-ec2.pem -o userknownhostsfile=known_hosts/$user \
+scp -i bch20170503-ec2.pem \
+  -o UserKnownHostsFile=known_hosts/$user \
+  -o CheckHostIP=no \
   -o StrictHostKeyChecking=yes \
   ${1+"$@"} "ubuntu@ssh.$user.stack.probcomp.net:$source" "$dest"
