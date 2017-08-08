2618dc36b5f69ecc99c987ed8d329a99b9a8ab9f
Aaron Vinson <avinson.public@gmail.com>
2017-08-08 01:11:24 -0700

    messing around with jupyter/docker-stack stuff.. prob not going to work without conda packages for probcomp

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
 
-#. /venv/bin/activate
+# activate python2 environment
+source activate python2
+
 #cp -r /branding ~/.jupyter/custom
-if [ ! -f /notebook/satellites-predictive.ipynb ]; then
-  cd /tmp && wget --progress=dot:giga -O - https://${CONTENT_URL} | gunzip -c | tar xf -
-  mv notebook/* /notebook/
+if [ ! -f ~/work/satellites-predictive.ipynb ]; then
+  mkdir -p ~/tmp
+  cd ~/tmp && wget --progress=dot:giga -O - https://${CONTENT_URL} | gunzip -c | tar xf -
+  mv ~/tmp/notebook/* ~/work/
+  rm -r ~/tmp
 fi
 
-cd /notebook
-exec /usr/local/bin/jupyter notebook --ip='*' --port=8080 --no-browser --NotebookApp.token= \
-  --allow-root --NotebookApp.iopub_data_rate_limit=10000000000
+exec "$@"
